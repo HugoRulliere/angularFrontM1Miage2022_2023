bc712e1d1992701a84b66527752ccd16a19a2cef
HugoRulliere <hugo.rulliere@etu.univ-cotedazur.fr>
2022-10-12 11:28:22 +0200

    fin tp1

---
+++ assignment-app/src/app/assignments/assignments.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Assignment } from './assignment.model';
+
+@Component({
+  selector: 'app-assignments',
+  templateUrl: './assignments.component.html',
+  styleUrls: ['./assignments.component.css']
+})
+export class AssignmentsComponent implements OnInit {
+
+  titre = "Mon application sur les Assignments";
+  ajoutActive = false;
+  nomDevoir:string = "";
+  
+  assignments:Assignment[] = [
+    {
+      nom: "Devoir Angular à rendre",
+      dateDeRendu: new Date('2022-10-10'),
+      rendu:false
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.ajoutActive = true
+    }, 2000);
+  }
+
+  onSubmit() {
+    const newAssignment = new Assignment();
+    newAssignment.nom = this.nomDevoir;
+    newAssignment.dateDeRendu = this.dateDeRendu;
+    newAssignment.rendu = false;
+    
+    this.assignments.push(newAssignment);
+  }
+
+}
